daa1e0e4af6c9ac7f73a8877e5d29f0e5493300a
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-03-05 11:40:56 +0900

    Initial commit

---
+++ todoapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+from .views import TaskList, TaskDetail, TaskCreate, TaskDelete, TaskUpdate, TastListLoginView, RegisterTodoApp
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('', TaskList.as_view(), name="tasks"),
+    path('task/<int:pk>/', TaskDetail.as_view(), name="task"),
+    path('create-task/', TaskCreate.as_view(), name="create-task"),
+    path('edit-task/<int:pk>/', TaskUpdate.as_view(), name="edit-task"),
+    path('delete-task/<int:pk>/', TaskDelete.as_view(), name="delete-task"),
+    path('login', TastListLoginView.as_view(), name="login"),
+    path('logout', LogoutView.as_view(next_page="login"), name="logout"),
+    path('register', RegisterTodoApp.as_view(), name="register"),
+]
---
+++ todoproject/settings.py
@@ -0,0 +1,87 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = 'django-insecure-+i#@$&=6#=yku-pr=d#yu79%_wsu1-21t%siy_%-v^@(qs%2^l'
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'todoapp',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'todoproject.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': ["templates"],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'todoproject.wsgi.application'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+LANGUAGE_CODE = 'ja'
+
+TIME_ZONE = 'Asia/Tokyo'
+
+USE_I18N = True
+
+USE_TZ = True
+
+LOGIN_URL = "login"
+
+STATIC_URL = 'static/'
+STATIC_DIRS = [BASE_DIR / "static"]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
